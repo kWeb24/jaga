cb497536a2f009ebfe5c0e9a80cb4e54421427d7
Kamil Weber <kamilweber24@gmail.com>
2017-09-30 18:20:50 +0200

    Add: KeywordSampleManager class skeleton

---
+++ modules/KeywordSampleManager/KeywordSampleManager.php
@@ -0,0 +1,29 @@
+<?php
+
+class KeywordSampleManager {
+  public function __construct() {
+
+  }
+
+  public function save() {
+
+  }
+
+  public function delete() {
+
+  }
+
+  public function load() {
+
+  }
+
+  public function checkoutSample() {
+
+  }
+
+  public function checkoutDir() {
+
+  }
+}
+
+?>
